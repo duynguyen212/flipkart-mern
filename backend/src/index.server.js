33cbb93e376e711f64964b87fb819b552278afb5
Duy NGUYEN <duynguyen212@gmail.com>
2021-03-03 17:29:57 +0100

    Adding User Model and User Route into MongoDB

--- backend/src/index.server.js
+++ backend/src/index.server.js
@@ -1,29 +1,28 @@
 var express = require('express')
 var env = require('dotenv')
 var bodyParser = require('body-parser')
+const cors = require("cors")
 var mongoose = require('mongoose');
-const url = 'mongodb+srv://lets_travel_admin:thietTaoLao%2369@cluster0.49fhw.mongodb.net/lets_travel?retryWrites=true&w=majority';
-
+// const url = `mongodb+srv://${process.env.MONGO_DB_USER}:${process.env.MONGO_DB_PASSWORD}@cluster0.blzhs.mongodb.net/${process.env.MONGO_DB_DATABASE}?retryWrites=true&w=majority`
+// string 'mongodb+srv://root:admin4231@cluster0.blzhs.mongodb.net/ecommerce_flipkart?retryWrites=true&w=majority'
 var app = express()
 
 app.use(bodyParser.json())
+//app.use(express.json())
+
+// routes
+const userRoutes = require('../routes/user')
 
 env.config()
 
-ongoose.connect(url,
-    {useNewUrlParser: true, useUnifiedTopology: true});
-
-app.get('/', (req, res, next) => {
-    res.status(200).json({
-        message: 'Hello from server Node JS'
+mongoose.connect
+    ( process.env.MONGODB_URI,
+        { useNewUrlParser: true, useUnifiedTopology: true , useCreateIndex: true }
+    ).then( () => {
+        console.log('Connected to MongoDB successfully!')        
     })
-})
 
-app.post('/data', (req, res, next) => {
-    res.status(200).json({
-        message: req.body
-    })
-})
+app.use('/api', userRoutes)    
 
 app.listen(process.env.PORT, () => {
     console.log(`Server is running on port ${process.env.PORT}`)
