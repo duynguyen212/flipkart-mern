9d7ef7bdd23692061aa83bb2ce9b129046dc5395
Duy NGUYEN <duynguyen212@gmail.com>
2021-03-06 18:01:22 +0100

    Adding Front-End For Admin Layout in React JS

---
+++ admin-app/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import { Jumbotron } from 'react-bootstrap';
+
+function App() {
+  return (
+    <div className="App">
+      <Jumbotron>
+        <h1>Admin Dashboard</h1>
+      </Jumbotron>
+    </div>
+  );
+}
+
+export default App;
